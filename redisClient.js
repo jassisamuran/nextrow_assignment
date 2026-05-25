0ccac1041b6b19b7152494a354358fb6e068f3af
jassisamuran <samuran3132@gmail.com>
2026-05-26 05:09:11 +0530

    added redis client singlenton pattern

---
+++ redisClient.js
@@ -0,0 +1,24 @@
+const redis=require("redis");
+let client=null;
+
+async function getRedisClient() {
+    if(client && client.isReady)return client;
+
+    client=redis.createClient({
+        url=process.env.REDIS_URL || "redis://localhost:6379",
+        socket:{
+            reconnectStrategy:(redis)=>Math.min(redis*100,3000)
+        }
+    })
+
+
+    client.on("error",(err)=>console.log("Redis error",error));
+    client.on("connect",()=>console.log("Redis connected"))
+    client.on("reconnecting",()=>console.log("Redis reconnecting..."))
+    
+    await client.connect();
+    return client();
+}
+
+
+module.exports={getRedisClient};
